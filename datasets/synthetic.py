e16e8808563321def0d55c3f1ef6e1467cd676a7
Eldar Abraham <53745147+eldarab@users.noreply.github.com>
2021-07-07 10:48:24 +0300

    moved synthetic.py to ./datasets

--- datasets/synthetic.py
+++ datasets/synthetic.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader, Dataset
 
 class SyntheticDataset(Dataset):
     def __init__(self, num_samples=1000, input_dim=784, num_classes=10):
+        # default input_dim inspired by MNIST 784=28x28
         self.num_samples = num_samples
         self.x = torch.randn(size=(num_samples, input_dim))
         self.y = torch.randint(low=0, high=num_classes - 1, size=(num_samples,))
